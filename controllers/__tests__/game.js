df5139b25e452e99fe6f0bdcad4f053abf7473a6
millarmcilwain <124205585+millarmcilwain@users.noreply.github.com>
2023-05-25 21:32:41 +0100

    verify GameId tests created

--- controllers/__tests__/game.js
+++ controllers/__tests__/game.js
@@ -1,22 +1,59 @@
-const gameController = require("../game");
+const gameController = require('../game');
 
 const mockId = 'fda56100-0ddb-4f06-9ea4-7c1919ff6d2f';
-jest.mock("uuid", () => ({ v4: () => mockId }));
+jest.mock('uuid', () => ({ v4: () => mockId }));
 
 
-describe("game controller", () => {
-    describe("createGame", () => {
-      it("Should return identifier when game created", () => {
-        const req = {};
-        const res = {
-            send: jest.fn()
-        };
+describe('game controller', () => {
+  describe('createGame', () => {
+    it('Should return identifier when game created', () => {
+      const req = {};
+      const res = {
+        send: jest.fn(),
+      };
 
-        gameController.createGame(req, res);
+      gameController.createGame(req, res);
 
-        expect(res.send).toHaveBeenCalledTimes(1);
-        expect(res.send).toHaveBeenCalledWith(id);
-      });
-    })
+      expect(res.send).toHaveBeenCalledTimes(1);
+      expect(res.send).toHaveBeenCalledWith(mockId);
+    });
+  });
+
+
+  describe('verifyGameID', () => {
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const next = jest.fn();
+    gameController.games[mockId] = {};
+
+    it('should call next() if the gameId exists', () => {
+      const req = { params: { gameId: mockId } };
+     
+      
+      gameController.verifyGameID(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.json).not.toHaveBeenCalled();
+
+    });
+    
+    it ('should send a JSON response with a 404 status if the gameID does not exist', ()=> {
+      
+      const req = { params: { gameId: '123' } };
+      const games = {}
+
+      gameController.verifyGameID(req, res, next);
+      
+      expect(next).not.toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({Message: 'Game ID does not exist.'});
+      
+
+    });
+
+  });
 });
-
--- controllers/game.js
+++ controllers/game.js
@@ -1,19 +1,18 @@
-const { v4: uuid } = require("uuid");
+const { v4: uuid } = require('uuid');
 
-
-const words = ["Banana", "Canine", "Unosquare", "Airport"];
+const words = ['Banana', 'Canine', 'Unosquare', 'Airport'];
 const games = {};
-const GUESS_LIMIT= 6;
+const GUESS_LIMIT = 6;
 
-const retrieveWord = () => words[Math.floor(Math.random()*words.length)];
+const retrieveWord = () => words[Math.floor(Math.random() * words.length)];
 
 const clearUnmaskedWord = (game) => {
-    const withoutUnmasked = { 
-        ...game,
-    };
-    delete withoutUnmasked.unmaskedWord;
-    return withoutUnmasked;
-}
+  const withoutUnmasked = {
+    ...game,
+  };
+  delete withoutUnmasked.unmaskedWord;
+  return withoutUnmasked;
+};
 
 const createGame = (req, res) => {
   const newGameWord = retrieveWord();
@@ -21,62 +20,67 @@ const createGame = (req, res) => {
   const newGame = {
     remainingGuesses: GUESS_LIMIT,
     unmaskedWord: newGameWord,
-    word: newGameWord.replaceAll(/[a-zA-Z0-9]/g, "_"),
-    status: "In Progress",
+    word: newGameWord.replaceAll(/[a-zA-Z0-9]/g, '_'),
+    status: 'In Progress',
     incorrectGuesses: [],
   };
 
   games[newGameId] = newGame;
   res.send(newGameId);
+};
+
+function getGame(req, res) {
+  const { gameId } = req.params;
+  if (!gameId) return res.sendStatus(404);
+
+  var game = games[gameId];
+  if (!game) {
+    return res.sendStatus(404);
+  }
+
+  res.status(200).json(clearUnmaskedWord(game));
 }
 
-function getGame(req, res) { 
-    const { gameId } = req.params;
-    if (!gameId) return res.sendStatus(404);
+function createGuess(req, res) {
+  const { gameId } = req.params;
+  const { letter } = req.body;
 
-    var game = games[gameId];
-    if (!game) {
-        return res.sendStatus(404); 
-    }
+  if (!gameId) return res.sendStatus(404);
 
-    res.status(200).json(clearUnmaskedWord(game));
+  var game = games[gameId];
+  if (!game) return res.sendStatus(404);
+
+  if (!letter || letter.length != 1) {
+    return res.status(400).json({
+      Message: 'Guess must be supplied with 1 letter',
+    });
+  }
+
+  
+
+  // todo: add logic for making a guess, modifying the game and updating the status
+
+  return res.status(200).json(clearUnmaskedWord(game));
 }
 
-function createGuess(req, res) { 
-    const { gameId } = req.params
-    const { letter } = req.body;
-
-    if (!gameId) return res.sendStatus(404);
-
-    var game = games[gameId];
-    if (!game) return res.sendStatus(404); 
-
-    if (!letter || letter.length != 1) {
-        return res.status(400).json({
-            Message: "Guess must be supplied with 1 letter"
-        })
-    }
-
-    // todo: add logic for making a guess, modifying the game and updating the status
-
-    return res.status(200).json(clearUnmaskedWord(game));
-}
-
-
+//check if body exists, convert to lower case, check body length? only take first value ensure letter value exists
 
 const verifyGameID = (req, res, next) => {
+  const { gameId } = req.params;
 
-    const { gameId } = req.params
+  Object.hasOwn(games, gameId)
+    ? next()
+    : res.status(404).json({
+        Message: 'Game ID does not exist.',
+      });
+};
 
-    Object.hasOwn(games, gameId) ? next() : res.status(400).json({
-        Message: 'Game ID does not exist.'
-    });
 
-}
 
 module.exports = {
-    createGame,
-    getGame,
-    createGuess,
-    verifyGameID
-  };
+  games,
+  createGame,
+  getGame,
+  createGuess,
+  verifyGameID,
+};
