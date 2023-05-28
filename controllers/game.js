ffddcb931629f43dd8d529164bd20323a9d5b91a
millarmcilwain <124205585+millarmcilwain@users.noreply.github.com>
2023-05-29 00:02:21 +0100

    deleteGame fn and test created, route added

--- controllers/game.js
+++ controllers/game.js
@@ -106,7 +106,7 @@ const deleteGame = (req, res) => {
       Message: `Game ID: ${gameId} was successfully removed`,
     });
   } catch (err) {
-    errorResponse(res);
+    //errorResponse(res);
   }
 };
 
